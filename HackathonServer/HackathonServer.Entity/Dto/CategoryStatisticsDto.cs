1db015f0aae4a7d0b010ad2c69c39fbce4a063c3
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-05-29 01:26:00 +0300

    Get User Category Statistics backend methods

---
+++ HackathonServer/HackathonServer.Entity/Dto/CategoryStatisticsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonServer.Entity.Dto
+{
+    public class CategoryStatisticsDto
+    {
+        public string CategoryName { get; set; }
+        public double Quantity { get; set; }
+    }
+}
