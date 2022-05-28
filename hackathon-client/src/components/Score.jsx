a8a9ae9cc30f4793197b9cb9667f5615dbec76a8
mustafahincal <mustafahncal@gmail.com>
2022-05-29 00:21:45 +0300

    add style, create score page

---
+++ hackathon-client/src/components/Score.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import adam from "../assets/adam.jpg";
+
+function Score() {
+   return (
+      <div>
+         <div className="score-container">
+            <div className="score-left">
+               <div className="score-inner">
+                  <h1 className="text-2xl font-bold mb-2">Müzeler</h1>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Porro eaque quod exercitationem laborum atque odio, explicabo
+                  nulla voluptas, consequ Lorem ipsum, dolor sit amet
+               </div>
+            </div>
+            <div className="score-right">
+               <img src={adam} alt="resim" className="image" />
+            </div>
+         </div>
+
+         <div className="score-container">
+            <div className="score-left">
+               <div className="score-inner">
+                  <h1 className="text-2xl font-bold mb-2">Müzeler</h1>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Porro eaque quod exercitationem laborum atque odio, explicabo
+                  nulla voluptas, consequ Lorem ipsum, dolor sit amet
+               </div>
+            </div>
+            <div className="score-right">
+               <img src={adam} alt="resim" className="image" />
+            </div>
+         </div>
+
+         <div className="score-container">
+            <div className="score-left">
+               <div className="score-inner">
+                  <h1 className="text-2xl font-bold mb-2">Müzeler</h1>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Porro eaque quod exercitationem laborum atque odio, explicabo
+                  nulla voluptas, consequ Lorem ipsum, dolor sit amet
+               </div>
+            </div>
+            <div className="score-right">
+               <img src={adam} alt="resim" className="image" />
+            </div>
+         </div>
+
+         <div className="score-container">
+            <div className="score-left">
+               <div className="score-inner">
+                  <h1 className="text-2xl font-bold mb-2">Müzeler</h1>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Porro eaque quod exercitationem laborum atque odio, explicabo
+                  nulla voluptas, consequ Lorem ipsum, dolor sit amet
+               </div>
+            </div>
+            <div className="score-right">
+               <img src={adam} alt="resim" className="image" />
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default Score;
