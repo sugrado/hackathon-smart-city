47e48a075c443ab1521f7e3303882e5e03d8ef54
mustafahincal <mustafahncal@gmail.com>
2022-05-29 09:24:28 +0300

    update newControlCenter component

--- hackathon-client/src/components/NewCenterRecord.jsx
+++ hackathon-client/src/components/NewCenterRecord.jsx
@@ -3,107 +3,107 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 function NewCenterRecord() {
-  const [mail, setMail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
-  const [neighbourhoods, setNeighbourhoods] = useState([]);
-  const [capacity, setCapacity] = useState("");
+   const [mail, setMail] = useState("");
+   const [phone, setPhone] = useState("");
+   const [address, setAddress] = useState("");
+   const [neighbourhoods, setNeighbourhoods] = useState([]);
+   const [capacity, setCapacity] = useState("");
 
-  useEffect(() => {
-    axios
-      .get("https://608e-178-241-52-165.eu.ngrok.io/api/neighbourhoods")
-      .then((res) => {
-        setNeighbourhoods(res.data.data);
-      });
-  }, []);
-  var object = {};
+   useEffect(() => {
+      axios
+         .get("https://608e-178-241-52-165.eu.ngrok.io/api/neighbourhoods")
+         .then((res) => {
+            setNeighbourhoods(res.data.data);
+         });
+   }, []);
+   var object = {};
 
-  const handleCenterRecord = (e) => {
-    e.preventDefault();
-    object.neighbourhoodId = getSelectedValue("neighbourhoodId");
-    object.address = address;
-    object.email = mail;
-    object.phoneNumber = phone;
-    object.capacity = Number(capacity);
-    axios
-      .post(
-        "https://608e-178-241-52-165.eu.ngrok.io/api/wasteCenters/addWasteCenter",
-        object
-      )
-      .then(
-        (res) => {
-          if (res.data.success) {
-            toast.success(res.data.message);
-          }
-        },
-        (err) => {
-          toast.error(err.response.data.message);
-        }
-      );
-  };
-  function getSelectedValue(elementId) {
-    const installments = document.querySelector(`#${elementId}`);
-    const selectedIndex = [installments.selectedIndex];
-    return installments.children[selectedIndex].value;
-  }
-  return (
-    <div>
-      <div className="text-2xl font-bold text-center">Atık Merkezi Ekle</div>
+   const handleCenterRecord = (e) => {
+      e.preventDefault();
+      object.neighbourhoodId = getSelectedValue("neighbourhoodId");
+      object.address = address;
+      object.email = mail;
+      object.phoneNumber = phone;
+      object.capacity = Number(capacity);
+      axios
+         .post(
+            "https://608e-178-241-52-165.eu.ngrok.io/api/wasteCenters/addWasteCenter",
+            object
+         )
+         .then(
+            (res) => {
+               if (res.data.success) {
+                  toast.success(res.data.message);
+               }
+            },
+            (err) => {
+               toast.error(err.response.data.message);
+            }
+         );
+   };
+   function getSelectedValue(elementId) {
+      const installments = document.querySelector(`#${elementId}`);
+      const selectedIndex = [installments.selectedIndex];
+      return installments.children[selectedIndex].value;
+   }
+   return (
+      <div>
+         <div className="text-2xl font-bold text-center">Atık Merkezi Ekle</div>
 
-      <form onSubmit={(e) => handleCenterRecord(e)}>
-        <div className="w-1/2 m-auto mt-5 flex flex-col">
-          <input
-            type="text"
-            value={mail}
-            onChange={(e) => setMail(e.target.value)}
-            placeholder="E-Posta"
-            className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
-          />
+         <form onSubmit={(e) => handleCenterRecord(e)}>
+            <div className="w-1/2 container-newCenter flex flex-col">
+               <input
+                  type="text"
+                  value={mail}
+                  onChange={(e) => setMail(e.target.value)}
+                  placeholder="E-Posta"
+                  className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
+               />
 
-          <input
-            type="text"
-            value={capacity}
-            onChange={(e) => setCapacity(e.target.value)}
-            placeholder="Kapasite"
-            className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
-          />
+               <input
+                  type="text"
+                  value={capacity}
+                  onChange={(e) => setCapacity(e.target.value)}
+                  placeholder="Kapasite"
+                  className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
+               />
 
-          <select
-            name="neighbourhoodId"
-            id="neighbourhoodId"
-            className="w-full outline-none border-2 py-1 px-3 mb-2"
-          >
-            <option value="0">Mahalle...</option>
-            {neighbourhoods.map((res) => {
-              return (
-                <option key={res.id} value={res.id}>
-                  {res.name}
-                </option>
-              );
-            })}
-          </select>
+               <select
+                  name="neighbourhoodId"
+                  id="neighbourhoodId"
+                  className="w-full outline-none border-2 py-1 px-3 mb-2"
+               >
+                  <option value="0">Mahalle...</option>
+                  {neighbourhoods.map((res) => {
+                     return (
+                        <option key={res.id} value={res.id}>
+                           {res.name}
+                        </option>
+                     );
+                  })}
+               </select>
 
-          <input
-            type="text"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            placeholder="Adres"
-            className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
-          />
+               <input
+                  type="text"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  placeholder="Adres"
+                  className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
+               />
 
-          <input
-            type="text"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            placeholder="Telefon"
-            className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
-          />
+               <input
+                  type="text"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  placeholder="Telefon"
+                  className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
+               />
 
-          <button className="btn self-end mt-4">Kaydet</button>
-        </div>
-      </form>
-    </div>
-  );
+               <button className="btn self-end mt-4">Kaydet</button>
+            </div>
+         </form>
+      </div>
+   );
 }
 
 export default NewCenterRecord;
--- hackathon-client/src/components/NewWasteRecord.jsx
+++ hackathon-client/src/components/NewWasteRecord.jsx
@@ -4,144 +4,144 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function NewWasteRecord() {
-  const [identityNumber, setIdentityNumber] = useState("");
-  const [unitSize, setUnitSize] = useState("");
-  const [products, setProducts] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [wasteCenters, setWasteCenters] = useState([]);
-  var object = {};
-  useEffect(() => {
-    axios
-      .get("https://608e-178-241-52-165.eu.ngrok.io/api/categories")
-      .then(({ data: response }) => setCategories(response.data));
-    axios
-      .get("https://608e-178-241-52-165.eu.ngrok.io/api/wasteCenters")
-      .then(({ data: response }) => setWasteCenters(response.data));
-  }, []);
+   const [identityNumber, setIdentityNumber] = useState("");
+   const [unitSize, setUnitSize] = useState("");
+   const [products, setProducts] = useState([]);
+   const [categories, setCategories] = useState([]);
+   const [wasteCenters, setWasteCenters] = useState([]);
+   var object = {};
+   useEffect(() => {
+      axios
+         .get("https://608e-178-241-52-165.eu.ngrok.io/api/categories")
+         .then(({ data: response }) => setCategories(response.data));
+      axios
+         .get("https://608e-178-241-52-165.eu.ngrok.io/api/wasteCenters")
+         .then(({ data: response }) => setWasteCenters(response.data));
+   }, []);
 
-  const handleAdminFormSubmit = (e) => {
-    e.preventDefault();
-    object.categoryId = getSelectedValue("categoryId");
-    object.wasteCenterId = getSelectedValue("wasteCenterId");
-    object.citizenIdentityNumber = identityNumber;
-    object.unitSize = unitSize;
-    setProducts([...products, object]);
-  };
+   const handleAdminFormSubmit = (e) => {
+      e.preventDefault();
+      object.categoryId = getSelectedValue("categoryId");
+      object.wasteCenterId = getSelectedValue("wasteCenterId");
+      object.citizenIdentityNumber = identityNumber;
+      object.unitSize = unitSize;
+      setProducts([...products, object]);
+   };
 
-  function getSelectedValue(elementId) {
-    const installments = document.querySelector(`#${elementId}`);
-    const selectedIndex = [installments.selectedIndex];
-    return installments.children[selectedIndex].value;
-  }
+   function getSelectedValue(elementId) {
+      const installments = document.querySelector(`#${elementId}`);
+      const selectedIndex = [installments.selectedIndex];
+      return installments.children[selectedIndex].value;
+   }
 
-  const handleNewWasteRecord = () => {
-    axios
-      .post(
-        "https://608e-178-241-52-165.eu.ngrok.io/api/wasteRecords/bulkInsert",
-        products
-      )
-      .then(
-        (res) => {
-          toast.success(res.data.message);
-          setProducts([]);
-        },
-        (err) => {
-          toast.error(err.response.data.message);
-        }
-      );
-  };
+   const handleNewWasteRecord = () => {
+      axios
+         .post(
+            "https://608e-178-241-52-165.eu.ngrok.io/api/wasteRecords/bulkInsert",
+            products
+         )
+         .then(
+            (res) => {
+               toast.success(res.data.message);
+               setProducts([]);
+            },
+            (err) => {
+               toast.error(err.response.data.message);
+            }
+         );
+   };
 
-  return (
-    <div>
-      <div className="text-2xl font-bold text-center bg-gray-50 ">
-        Yeni Atık Kaydet
-      </div>
-      <div className="panel-container">
-        <div className="left-panel">
-          <form onSubmit={(e) => handleAdminFormSubmit(e)}>
-            <div className="w-full mt-5 flex flex-col">
-              <input
-                type="text"
-                value={identityNumber}
-                onChange={(e) => setIdentityNumber(e.target.value)}
-                placeholder="Kullanıcı TC Kimlik Numarası"
-                className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
-              />
+   return (
+      <div>
+         <div className="text-2xl font-bold text-center bg-gray-50 ">
+            Yeni Atık Kaydet
+         </div>
+         <div className="panel-container">
+            <div className="left-panel">
+               <form onSubmit={(e) => handleAdminFormSubmit(e)}>
+                  <div className="w-full mt-5 flex flex-col">
+                     <input
+                        type="text"
+                        value={identityNumber}
+                        onChange={(e) => setIdentityNumber(e.target.value)}
+                        placeholder="Kullanıcı TC Kimlik Numarası"
+                        className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
+                     />
 
-              <select
-                name="categoryId"
-                id="categoryId"
-                className="w-full outline-none border-2 py-1 px-3 mb-2"
-              >
-                <option value="0">Kategori</option>
-                {categories.map((res) => {
-                  return (
-                    <option key={res.id} value={res.id}>
-                      {res.name}
-                    </option>
-                  );
-                })}
-              </select>
-              <select
-                name="wasteCenterId"
-                id="wasteCenterId"
-                className="w-full outline-none border-2 py-1 px-3 mb-2"
-              >
-                <option value="0">Atık Merkezi...</option>
-                {wasteCenters.map((res) => {
-                  return (
-                    <option key={res.id} value={res.id}>
-                      {res.name}
-                    </option>
-                  );
-                })}
-              </select>
+                     <select
+                        name="categoryId"
+                        id="categoryId"
+                        className="w-full outline-none border-2 py-1 px-3 mb-2"
+                     >
+                        <option value="0">Kategori</option>
+                        {categories.map((res) => {
+                           return (
+                              <option key={res.id} value={res.id}>
+                                 {res.name}
+                              </option>
+                           );
+                        })}
+                     </select>
+                     <select
+                        name="wasteCenterId"
+                        id="wasteCenterId"
+                        className="w-full outline-none border-2 py-1 px-3 mb-2"
+                     >
+                        <option value="0">Atık Merkezi...</option>
+                        {wasteCenters.map((res) => {
+                           return (
+                              <option key={res.id} value={res.id}>
+                                 {res.name}
+                              </option>
+                           );
+                        })}
+                     </select>
 
-              <input
-                type="text"
-                value={unitSize}
-                onChange={(e) => setUnitSize(e.target.value)}
-                placeholder="Materyal Ağırlığı"
-                className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
-              />
+                     <input
+                        type="text"
+                        value={unitSize}
+                        onChange={(e) => setUnitSize(e.target.value)}
+                        placeholder="Materyal Ağırlığı"
+                        className="w-full outline-none py-1 px-3 border-2 rounded mb-2"
+                     />
 
-              <button className="btn self-end mt-4">Ekle</button>
+                     <button className="btn self-end mt-4">Listeye Ekle</button>
+                  </div>
+               </form>
             </div>
-          </form>
-        </div>
-        <div className="right-panel">
-          <div>
-            {products.length < 1 ? (
-              <p className="text-lg p-2">Liste boş</p>
-            ) : (
-              products.map((product, index) => (
-                <div
-                  key={index}
-                  className="py-2 px-3 bg-buyuksehir text-black flex justify-between mb-2"
-                >
-                  <span>
-                    {
-                      categories.find(
-                        (a) => a.id === Number(product.categoryId)
-                      ).name
-                    }
-                    &nbsp; Atığı
-                  </span>
-                  <span>{product.unitSize} kg</span>
-                </div>
-              ))
-            )}
-          </div>
-          <button
-            className="btn self-end"
-            onClick={() => handleNewWasteRecord()}
-          >
-            İşlemi Gerçekleştir
-          </button>
-        </div>
+            <div className="right-panel">
+               <div>
+                  {products.length < 1 ? (
+                     <p className="text-lg p-2">Liste boş</p>
+                  ) : (
+                     products.map((product, index) => (
+                        <div
+                           key={index}
+                           className="py-2 px-3 bg-buyuksehir text-black flex justify-between mb-2"
+                        >
+                           <span>
+                              {
+                                 categories.find(
+                                    (a) => a.id === Number(product.categoryId)
+                                 ).name
+                              }
+                              &nbsp; Atığı
+                           </span>
+                           <span>{product.unitSize} kg</span>
+                        </div>
+                     ))
+                  )}
+               </div>
+               <button
+                  className="btn self-end"
+                  onClick={() => handleNewWasteRecord()}
+               >
+                  İşlemi Gerçekleştir
+               </button>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default NewWasteRecord;
